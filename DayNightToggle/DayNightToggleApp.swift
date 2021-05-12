0e3346f1b5d8fc18cbb69372e17275cb03a41ce1
Matthew <matthew@enigmalabs.com>
2021-05-12 23:17:39 +0100

    Initial Commit

---
+++ DayNightToggle/DayNightToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayNightToggleApp.swift
+//  DayNightToggle
+//
+//  Created by Matthew Chukwuemeka on 12/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct DayNightToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
